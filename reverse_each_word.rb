5517043f1ec721da9d6d7ca8723dba9520c0329a
Rachel T <rachmaybe@gmail.com>
2019-08-12 14:50:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
-# def reverse_each_word(sentence1, sentence2)
-#   counter = 0
-#   while counter < sentence1.length 
-#   reverse - string[counter] + reverse
-#   index +=1
-#   end
-#   puts reverse
-# end 
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
