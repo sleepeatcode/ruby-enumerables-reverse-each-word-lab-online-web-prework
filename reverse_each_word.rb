b683f63e842398fefa5414c803859645316e327e
Rachel T <rachmaybe@gmail.com>
2019-08-12 14:39:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(sentence1, sentence2)
-  counter = 0
-  while counter < sentence1.length 
-   reverse - string[counter] + reverse
-   index +=1
-  end
-  puts reverse
-end 
+# def reverse_each_word(sentence1, sentence2)
+#   counter = 0
+#   while counter < sentence1.length 
+#   reverse - string[counter] + reverse
+#   index +=1
+#   end
+#   puts reverse
+# end 
